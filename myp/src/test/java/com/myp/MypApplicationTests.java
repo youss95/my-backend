5125eaf874b6d955a4cc090945ab90a7bf0f918d
sykim <sykim@skyware.co.kr>
2023-03-20 14:54:59 +0900

    commit

---
+++ myp/src/test/java/com/myp/MypApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
